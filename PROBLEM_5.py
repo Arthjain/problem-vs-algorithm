9c82464c545f2da6f7fed941a07e5477f1a6627a
Arth jain <arthjainabc@gmail.com>
2020-06-01 22:26:52 +0530

    Update PROBLEM_5.py

--- PROBLEM_5.py
+++ PROBLEM_5.py
@@ -1,74 +1,94 @@
 class TrieNode:
-    def __init__(self, end_of_word=False):
-        self.end_of_word = end_of_word
-        self.char_dict = dict()
-
+    def __init__(self):
+        ## Initialize this node in the Trie
+        self.is_word = False
+        self.children = {}
+    
     def insert(self, char):
-        sub_char_node = TrieNode()
-        self.char_dict[char] = sub_char_node
-
-        return sub_char_node
-
-    def suffixes(self, suffix=''):
-        output_str_list = list()
-
-        def find_suffix(node, output_str):
-            if node.end_of_word:
-                output_str_list.append(output_str)
-            for char in node.char_dict:
-                temp_output_str = output_str + char
-                find_suffix(node.char_dict[char], temp_output_str)
-        find_suffix(self, "")
-        return output_str_list
-
-
+        ## Add a child node in this Trie
+        if char not in self.children:
+            self.children[char] = TrieNode()
+        else:
+            pass
+        
+## The Trie itself containing the root node and insert/find functions
 class Trie:
     def __init__(self):
-        root_node = TrieNode()
-        self.root = root_node
+        ## Initialize this Trie (add a root node)
+         self.root = TrieNode()
 
     def insert(self, word):
-        cur_node = self.root
+        ## Add a word to the Trie
+        node = self.root
+
         for char in word:
-            if char in cur_node.char_dict:
-                cur_node = cur_node.char_dict[char]
-            else:
-                new_child_node = cur_node.insert(char)
-                cur_node = new_child_node
-        cur_node.end_of_word = True
+            node.insert(char)
+            node = node.children[char]
+
+        node.is_word = True
 
     def find(self, prefix):
-        cur_node = self.root
+        ## Find the Trie node that represents this prefix
+        node = self.root
+
         for char in prefix:
-            if char in cur_node.char_dict:
-                cur_node = cur_node.char_dict[char]
-            else:
-                return None
+            if char not in node.children:
+                return False
+            node = node.children[char]
 
-        return cur_node
+        return node
+class TrieNode:
+    def __init__(self):
+        ## Initialize this node in the Trie
+        self.is_word = False
+        self.children = {}
+        
+    
+    def insert(self, char):
+        ## Add a child node in this Trie
+        if char not in self.children:
+            self.children[char] = TrieNode()
+        else:
+            pass
+        
+    def suffixes(self, suffix = ''):
+        ## Recursive function that collects the suffix for 
+        ## all complete words below this point
+        results = []
 
-    def __str__(self):
-        output_str = [""]
+        if self.is_word and suffix != '':
+            results.append(suffix)
 
-        def print_node(node, output_str):
-            output_str[0] += f"\nEnd of word: {node.end_of_word}\n"
-            for char in node.char_dict:
-                output_str[0] += f"char: {char}"
-                print_node(node.char_dict[char], output_str)
+        if len(self.children) == 0:
+            return results
 
-        print_node(self.root, output_str)
+        results = []
 
-        return output_str[0]
+        if self.is_word and suffix != '':
+            results.append(suffix)
 
+        for char in self.children:
+            results.extend(self.children[char].suffixes(suffix=suffix+char))
 
+        return results
 MyTrie = Trie()
 wordList = [
-    "ant", "anthology", "antagonist", "antonym",
-    "fun", "function", "factory",
+    "ant", "anthology", "antagonist", "antonym", 
+    "fun", "function", "factory", 
     "trie", "trigger", "trigonometry", "tripod"
 ]
 for word in wordList:
     MyTrie.insert(word)
-
-node = MyTrie.find('f')
-print(node.suffixes())
+from ipywidgets import widgets
+from IPython.display import display
+from ipywidgets import interact
+def f(prefix):
+    if prefix != '':
+        prefixNode = MyTrie.find(prefix)
+        if prefixNode:
+            print('\n'.join(prefixNode.suffixes()))
+        else:
+            print(prefix + " not found")
+    else:
+        print('')
+interact(f,prefix='');
